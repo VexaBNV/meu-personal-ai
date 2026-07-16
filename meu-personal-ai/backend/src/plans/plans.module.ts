6926e5c7623dc81dcb4629c613ab1c4b24baa58e
VexaBNV <vexabnv@gmail.com>
2026-07-15 21:32:18 -0300

    Update plans.module.ts

--- meu-personal-ai/backend/src/plans/plans.module.ts
+++ meu-personal-ai/backend/src/plans/plans.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlansController } from './plans.controller';
 import { PlansService } from './plans.service';
+import { User } from '../users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [PlansController],
   providers: [PlansService],
   exports: [PlansService],
